f95353ae986bcbdad8f4029902a7e4ed5ea59d7c
Jordan Whitten <jordanwa1947@gmail.com>
2018-09-18 09:18:51 -0600

    added tests and photograph class

---
+++ lib/photograph.rb
@@ -0,0 +1,10 @@
+class Photograph
+  attr_reader :id, :name, :artist_id, :year
+  def initialize(attributes_hash)
+    @id = attributes_hash[:id]
+    @name = attributes_hash[:name]
+    @artist_id = attributes_hash[:artist_id]
+    @year = attributes_hash[:year]
+  end 
+
+end
